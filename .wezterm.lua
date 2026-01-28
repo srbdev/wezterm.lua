118a3842c1a68e147cacbe1d3fb2c9b8ff36e74a
Sylvain <srbdev@users.noreply.github.com>
2026-01-27 21:07:52 -0700

    Modify wezterm configuration parameters
    Updated wezterm configuration settings including window padding, font size, and initial column width.

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,30 +1,31 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+  local wezterm = require 'wezterm'
 
--- This will hold the configuration.
-local config = wezterm.config_builder()
+  -- This will hold the configuration.
+  local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices.
+  -- This is where you actually apply your config choices.
 
--- config.window_decorations = "RESIZE"
-config.enable_tab_bar = false
-config.initial_cols = 180
-config.initial_rows = 55
+  -- config.window_decorations = "RESIZE"
+  config.enable_tab_bar = false
+  config.initial_cols = 190
+  config.initial_rows = 55
 
-config.window_padding = {
-  left = 15,
-  right = 15,
-  top = 20,
-  bottom = 1,
-}
+  config.window_padding = {
+    left = 10,
+    right = 10,
+    top = 10,
+    bottom = 1,
+  }
 
-config.color_scheme = 'nord'
+  config.color_scheme = 'nord'
 
-config.font_size = 11
-config.font = wezterm.font('BlexMono Nerd Font')
--- config.font = wezterm.font 'JetBrains Mono'
+  config.font_size = 12
+  -- config.font = wezterm.font('BlexMono Nerd Font')
+  -- config.font = wezterm.font 'JetBrains Mono'
+  config.font = wezterm.font 'IBM Plex Mono'
 
-config.window_background_opacity = 0.98
-config.macos_window_background_blur = 50
+  config.window_background_opacity = 0.92
+  config.macos_window_background_blur = 50
 
-return config
+  return config
